34063796b60404640c3d23683a34259e7ade0258
xiaomizhou <liuxmoo@gmail.com>
2018-06-07 09:47:30 +0800

    创建登录界面，

--- controllers/default.go
+++ controllers/default.go
@@ -4,12 +4,10 @@ import (
 	"github.com/astaxie/beego"
 )
 
-type MainController struct {
+type LoginController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+func (c *LoginController) Get() {
+	c.TplName = "login.html"
 }
